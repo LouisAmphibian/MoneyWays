eed04b30b0163297164826373e861622275850ca
ayanda <ayandamantshinga30@gmail.com>
2025-07-20 22:51:30 +0200

    Fixed the Nav bar  from overlaping with the list of button

--- app/src/main/java/com/example/moneywaysapp/DashboardActivity.kt
+++ app/src/main/java/com/example/moneywaysapp/DashboardActivity.kt
@@ -6,6 +6,8 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.moneyways.R
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class DashboardActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +30,17 @@ class DashboardActivity: AppCompatActivity() {
             //setContentView(R.layout.activity_add_expense)
             navigateToAddExpense()
         }
+
+        // BottomAppBar Match BottomNavigationView’s Actual Height
+        val bottomAppBar = findViewById<BottomAppBar>(R.id.bottomAppBar)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        bottomNav.post{
+            val navHeight = bottomNav.height
+            bottomAppBar.layoutParams.height = navHeight
+            bottomAppBar.requestLayout()
+        }
+
     }
 
     private fun navigateToAddExpense() {
